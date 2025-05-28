75ffc1b6c88aad41e8f2f033a8b6fd91e97a752c
takuya-ki <taku8926@gmail.com>
2025-05-28 22:09:58 +0900

    remove unnecessary lines from setup.py for hande_tutorials

--- ros2_ws/humble/src/hande_tutorials/setup.py
+++ ros2_ws/humble/src/hande_tutorials/setup.py
@@ -15,9 +15,6 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        (os.path.join('share', package_name, 'meshes/visual'), glob('meshes/visual/*')),
-        (os.path.join('share', package_name, 'meshes/collision'), glob('meshes/collision/*')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
     ],
     install_requires=['setuptools'],
